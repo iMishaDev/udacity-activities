3aa9521d3f7e9aa23bebbebb6988fca90acb2531
Mashael <m.alsaeedi@safcsp.org.sa>
2021-06-18 19:40:53 +0300

    initial version

--- flask-sqlalchemy-activity/app.py
+++ flask-sqlalchemy-activity/app.py
@@ -14,33 +14,12 @@ def index():
     return redirect(url_for('getWorkouts', workout_id=Workout.query.first().id))
     
 
+# helpful instructions 
 # You can use the with_entities() method to restrict which columns you'd like to return in the result.
-#   states = Venue.query.with_entities(
-#       Venue.city, Venue.state).distinct().all()
-#   query = Venue.query.options(
-#       load_only('name', 'id'), joinedload(Venue.shows))
-#   data = []
-#   for state in states: 
-#     venues = query.filter(Venue.city==state.city, Venue.state==state.state).all()
-#     formattedVenues = []
+# You can use  options( joinedload(Entity.Column))
+# filter == | filter_by(col=)
+# searching Entity.ilike('%'+textToSearch+'%'))
 
-#     for venue in venues:
-#       formattedVenues.append({
-#         'name': venue.name,
-#         'id': venue.id,
-#         'num_upcoming_shows': len(venue.shows)
-#       })
-#     data.append({
-#       'city': state.city,
-#       'state': state.state,
-#       'venues': formattedVenues
-#     })
-#  questions =  Venue.query.filter(Venue.name.ilike('%'+textToSearch+'%')).all()
-
-# past_shows = Show.query.filter(
-#     Show.venue_id == venue.id,  Show.start_time < todays_datetime).all()
-# upcoming_shows = Show.query.filter(
-#     Show.venue_id == venue.id,  Show.start_time > todays_datetime).all()
 
 
     
@@ -52,30 +31,9 @@ def get_sec(time_str):
 
 @app.route('/<workout_id>')
 def getWorkouts(workout_id):
-    workout = Workout.query.get(workout_id)
-    workoutsHistory = Workout.query.order_by(Workout.created_at.desc()).all()
-    user = User.query.first()
-    formatted_workouts = [w.format() for w in workoutsHistory]
-
-
-
-    todayActivities = Workout.query.filter(
-        Workout.created_at==date.today()).all()
-    todayActivitiesData = {'move': 0, 'excercise': 0, 'stand': 0}
-    for activity in todayActivities:
-        todayActivitiesData['move'] += int(activity.totalCal)
-        todayActivitiesData['excercise'] += get_sec(activity.totalTime)//60
-        todayActivitiesData['stand'] += 1
     
-    percentages = {'move': 0, 'excercise': 0, 'stand': 0}
-    percentages['move'] = min(math.floor(
-        todayActivitiesData['move']/user.moveGoal*100), 100)
-    percentages['excercise'] = min(math.floor(
-        todayActivitiesData['excercise']/user.excersiceGoal*100), 100)
-    percentages['stand'] = min(math.floor(
-        todayActivitiesData['stand']/user.standGoal*100) ,100)
 
-    return render_template('home.html',  workouts=formatted_workouts, percentages=percentages, today=todayActivitiesData, workout=workout, user=user)
+    return render_template('home.html',  workouts=[], percentages=[], today=[], workout={}, user={})
 
 
 @app.route('/start-workout', methods=['GET'])
@@ -121,21 +79,7 @@ def addActivity():
 @app.route('/start-workout', methods=['POST'])
 def addActivityPost():
     data = request.form
-    print(data)
-    workout = Workout(type=data['type'],
-                        activeCal=data['activeCal'],
-                        totalCal=data['totalCal'],
-                        totalTime=data['totalTime'],
-                        heart=data['heart'],
-                        icon='static/icons/'+data['type'].lower()+'.png',
-                        created_at=datetime.datetime.now())
-    workout.insert()
     return redirect(url_for('index'))
-
-
-# @app.route('/history', methods=['POST'])
-# def addActivity():
-#     data = Workout.query.group
 
 
 
