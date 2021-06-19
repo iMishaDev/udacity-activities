ce501b9a97626319c3778fa20d7921b5201411c8
Mashael <m.alsaeedi@safcsp.org.sa>
2021-06-19 22:34:55 +0300

    update the starter code

--- flask-sqlalchemy-activity/app.py
+++ flask-sqlalchemy-activity/app.py
@@ -5,16 +5,7 @@ from sqlalchemy.orm import load_only, joinedload
 from datetime import date
 import math
 
-
-app = Flask(__name__)
-setup_db(app)
-
-@app.route('/')
-def index():
-    return redirect(url_for('getWorkouts', workout_id=Workout.query.first().id))
-    
-
-# helpful instructions 
+# helpful instructions
 # You can use the with_entities() method to restrict which columns you'd like to return in the result.
 # You can use  options( joinedload(Entity.Column))
 # filter == | filter_by(col=)
@@ -22,18 +13,39 @@ def index():
 
 
 
-    
+app = Flask(__name__)
+setup_db(app)
+
+# this is a helper function, you are going to 
+# need it when you want to calculate the the 
+# total time spent working out during the day,
+# since the `totalTiem` is stored as a string.    
 def get_sec(time_str):
     """Get Seconds from time."""
     h, m, s = time_str.split(':')
     return int(h) * 3600 + int(m) * 60 + int(s)
 
+# here's the entry point which should redirect the user to getWorkouts endpoint,
+# as we mentioned in the rREADME we always have to return at leaset one workout fot the viewer
+@app.route('/')
+def index():
+    return redirect(url_for('getWorkouts', workout_id=Workout.query.first().id))
+
 
 @app.route('/<workout_id>')
 def getWorkouts(workout_id):
-    
-
-    return render_template('home.html',  workouts=[], percentages=[], today=[], workout={}, user={})
+    # fetch user's workouts history
+    # one workout details by the help of workout_id 
+    # user's personal data
+    # his daily activity showing today's activity ( how much the user moved, exercised and stand ) along with the percentage of achivement based on the user daily goal.
+    return render_template('home.html', 
+                        workouts=[],
+                        dailyActivities={'move': {'cals': 0, 'percentage': 0},
+                                        'excercise': {'cals': 0, 'percentage': 0},
+                                        'stand': {'cals': 0, 'percentage': 0}
+                                        },
+                        workout={},
+                        user={})
 
 
 @app.route('/start-workout', methods=['GET'])
